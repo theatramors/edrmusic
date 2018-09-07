433f96789ed39cc26fb6f5495c2fd78bf6e0f514
Фунтиков Вадим <vfuntikov@mmtr.ru>
2018-09-07 17:15:59 +0300

    Add jwt dependency, update authService

--- src/main/java/amors/service/impl/AuthServiceImpl.java
+++ src/main/java/amors/service/impl/AuthServiceImpl.java
@@ -2,13 +2,61 @@ package amors.service.impl;
 
 import amors.dto.AuthRequest;
 import amors.dto.AuthResponse;
+import amors.entity.User;
 import amors.service.api.AuthService;
+import amors.service.api.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+    private final UserService userService;
+
+    private Map<Long, ValidatedUserToken> tokens = new ConcurrentHashMap<>();
+
+    @Autowired
+    public AuthServiceImpl(UserService userService) {this.userService = userService;}
+
     @Override
     public AuthResponse authenticateUser(AuthRequest authRequest) {
-        return new AuthResponse("hUdshjDHSjsa8943jDSAd9sdaj32FSa", "Authentication success");
+        if (authRequest.getUsername() == null) {
+            return new AuthResponse("Authentication failed");
+        }
+        User user = userService.findByUsername(authRequest.getUsername());
+        if (user == null && matchesPass(authRequest, user)) {
+            return new AuthResponse("Invalid username");
+        }
+        String token = generateToken(user);
+        tokens.put(user.getId(), new ValidatedUserToken(token));
+        System.out.println("Tokens in safe: " + tokens.size());
+        return new AuthResponse(token, "Authentication success");
+    }
+
+    private Boolean matchesPass(AuthRequest authRequest, User user) {
+        return authRequest.getPassword().equals(user.getPassword());
+    }
+
+    private String generateToken(User user) {
+        Claims claims = Jwts.claims().setSubject(user.getUsername());
+        claims.put("id", user.getId());
+        claims.put("login", user.getUsername());
+        claims.setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000));
+
+        // TODO Replace deprecated method
+        return Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.HS512, "secret").compact();
+    }
+
+    private static class ValidatedUserToken {
+        private final String value;
+        private final Boolean valid = true;
+
+        private ValidatedUserToken(String value) {this.value = value;}
     }
 }
